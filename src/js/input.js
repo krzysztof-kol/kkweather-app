6dc192f28aafb841ef849c607e526bc3b3bdc15e
Kolesnik <krzysztof.kolesnik1@gmail.com>
2023-05-15 07:30:01 +0200

    Redefining smaller weather to details section

--- src/js/input.js
+++ src/js/input.js
@@ -1,15 +1,15 @@
 const h1Input = document.querySelector("#h1_input");
 
-function adjustWidth(input) {
+export function adjustWidth(input) {
   let numberOfCharacters = input.value.length;
 
   if (numberOfCharacters >= 7) {
     let length = numberOfCharacters + "ch";
-    input.style.width = length;
+    return (input.style.width = length);
   }
 
   if (numberOfCharacters <= 7) {
     let length = numberOfCharacters + "ch";
-    input.style.width = length;
+    return (input.style.width = length);
   }
 }

1b3de563c9c80e853c662e6d69f9158b8aae3eae
Kolesnik <krzysztof.kolesnik1@gmail.com>
2023-05-18 13:38:47 +0200

    Destructurizing objects in getWeather

--- src/js/getWeather.js
+++ src/js/getWeather.js
@@ -1,42 +1,3 @@
-// const successCallback = (position) => {
-//   console.log(position);
-// };
-
-// const errorCallback = (error) => {
-//   console.error("Nie udało się pobrać lokalizacji");
-// };
-
-// export function getLocation() {
-//   return new Promise((successCallback, errorCallback) => {
-//     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-//   });
-// }
-
-// const currentTempOnWebpage = document.querySelector("#current-temp");
-
-// async function getWeather() {
-//   try {
-//     const position = await getLocation();
-//     const latitude = position.coords.latitude;
-//     const longitude = position.coords.longitude;
-
-//     const timezone = await Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-//     const weather = await fetch(
-//       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=${timezone}`
-//     );
-//     // console.log(weather);
-//     const data = await weather.json();
-//     console.log(data);
-//     const currentTemp = await data.current_weather.temperature;
-//     currentTempOnWebpage.textContent = currentTemp;
-//   } catch (error) {
-//     console.log("Błąd podczas pobierania lokalizacji");
-//   }
-// }
-
-// getWeather();
-
 import "./getCoords.js";
 import { getCoordinates } from "./getCoords.js";
 
@@ -67,28 +28,27 @@ export async function getWeather() {
 
 export async function renderCurrentData() {
   const data = await getWeather();
-  const currentWeather = data.current_weather;
-  const dailyWeather = data.daily;
 
-  const currentTemp = currentWeather.temperature;
+  const {
+    current_weather: currentWeather,
+    daily: dailyWeather,
+    hourly: hourlyWeather,
+  } = data;
+  const { temperature: currentTemp, windspeed: currentWindSpeed } =
+    currentWeather;
+
+  const [currentHighTemp] = dailyWeather.temperature_2m_max;
+  const [currentLowTemp] = dailyWeather.temperature_2m_min;
+  const [currentFLHighTemp] = dailyWeather.apparent_temperature_max;
+  const [currentFLLowTemp] = dailyWeather.apparent_temperature_min;
+  const [currentPrecip] = hourlyWeather.precipitation;
+
   currentTempOnPage.textContent = currentTemp;
-
-  const currentWindSpeed = currentWeather.windspeed;
   currentWindSpeedOnPage.textContent = currentWindSpeed;
-
-  const currentHighTemp = dailyWeather.temperature_2m_max[0];
   currentTempHighOnPage.textContent = currentHighTemp;
-
-  const currentLowTemp = dailyWeather.temperature_2m_min[0];
   currentTempLowOnPage.textContent = currentLowTemp;
-
-  const currentFLHighTemp = dailyWeather.apparent_temperature_max[0];
   currentTempFLHighOnPage.textContent = currentFLHighTemp;
-
-  const currentFLLowTemp = dailyWeather.apparent_temperature_min[0];
   currentTempFLLowOnPage.textContent = currentFLLowTemp;
-
-  const currentPrecip = dailyWeather.apparent_temperature_min[0];
   currentPrecipOnPage.textContent = currentPrecip;
 }
 

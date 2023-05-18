d0bfd92ae518a0f16325a21bfef6b8368e7dfc8c
Kolesnik <krzysztof.kolesnik1@gmail.com>
2023-05-18 17:47:25 +0200

    btn test

--- src/js/toggles.js
+++ src/js/toggles.js
@@ -1,27 +1,11 @@
 const hourlyForecastButton = document.querySelector(".btn");
 const hourlySection = document.querySelector(".hourly-forecast__section");
 const buttonHourlyIcon = document.querySelector(".btn__icon");
-// hourlyForecastButton.addEventListener("click", isActive);
+hourlyForecastButton.addEventListener("click", toggleHourlyForecast);
 // hourlyForecastButton.addEventListener("click", toggleButton);
 
-// export function toggleHourlyForecast() {
-//   hourlySection.classList.toggle("active");
-//   hourlyForecastButton.classList.toggle("active btn-active");
-//   buttonHourlyIcon.classList.toggle("btn__icon-active");
-// }
-
-// export function toggleButton() {
-//   hourlyForecastButton.classList.toggle("active");
-// }
-
-hourlyForecastButton.addEventListener("click", changeColor);
-
-export function changeColor() {
-  const isActive = hourlyForecastButton.getAttribute("data-active") === "true";
-
-  if (isActive) {
-    hourlyForecastButton.removeAttribute("data-active");
-  } else {
-    hourlyForecastButton.setAttribute("data-active", "true");
-  }
+export function toggleHourlyForecast() {
+  hourlySection.classList.toggle("active");
+  buttonHourlyIcon.classList.toggle("btn__icon-active");
+  hourlyForecastButton.classList.toggle("btn-active");
 }

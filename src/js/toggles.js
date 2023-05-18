627d23a31badfd4b82377d9b29d0df9975ac5f7a
Kolesnik <krzysztof.kolesnik1@gmail.com>
2023-05-18 16:40:22 +0200

    Another button test

--- src/js/toggles.js
+++ src/js/toggles.js
@@ -2,15 +2,9 @@ const hourlyForecastButton = document.querySelector(".btn");
 const hourlySection = document.querySelector(".hourly-forecast__section");
 const buttonHourlyIcon = document.querySelector(".btn__icon");
 hourlyForecastButton.addEventListener("click", toggleHourlyForecast);
-hourlyForecastButton.addEventListener("click", toggleHourlyForecast);
-hourlyForecastButton.addEventListener("click", toggleHourlyForecast);
 
 export function toggleHourlyForecast() {
   hourlySection.classList.toggle("active");
   hourlyForecastButton.classList.toggle("btn-active");
   buttonHourlyIcon.classList.toggle("btn__icon-active");
-}
-
-export function toggleButton() {
-  hourlyForecastButton.classList.toggle("active");
 }

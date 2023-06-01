4a735dee212142569560a71251b7f065eb97ced1
Kolesnik <krzysztof.kolesnik1@gmail.com>
2023-06-01 15:11:07 +0200

    Fixing the button

--- src/js/toggles.js
+++ src/js/toggles.js
@@ -2,12 +2,28 @@ const hourlyForecastButton = document.querySelector(".btn");
 const hourlySection = document.querySelector(".hourly-forecast__section");
 const buttonHourlyIcon = document.querySelector(".btn__icon");
 hourlyForecastButton.addEventListener("click", toggleHourlyForecast);
+
+hourlyForecastButton.addEventListener("mouseover", () => {
+  buttonHourlyIcon.classList = "btn__icon btn__icon-active";
+  hourlyForecastButton.classList = "btn btn-active";
+});
+
+hourlyForecastButton.addEventListener("mouseout", () => {
+  buttonHourlyIcon.classList = "btn__icon ";
+  hourlyForecastButton.classList = "btn";
+});
+
+hourlyForecastButton.addEventListener("touchstart", () => {
+  buttonHourlyIcon.classList = "btn__icon btn__icon-active";
+  hourlyForecastButton.classList = "btn btn-active";
+});
+
+hourlyForecastButton.addEventListener("touchend", () => {
+  buttonHourlyIcon.classList = "btn__icon";
+  hourlyForecastButton.classList = "btn";
+});
 // hourlyForecastButton.addEventListener("click", buttonBlur);
 // hourlyForecastButton.addEventListener("click", toggleButton);
-
-export function buttonBlur(button) {
-  button.blur();
-}
 
 export function toggleHourlyForecast() {
   hourlySection.classList.toggle("active");
@@ -16,3 +32,5 @@ export function toggleHourlyForecast() {
   hourlyForecastButton.classList.toggle("btn-unfocus");
   // buttonBlur(hourlyForecastButton);
 }
+
+export function buttonHover() {}
